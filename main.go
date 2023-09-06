d2b138cf7a962d0c4b613f6df8a21be4fb01b002
Orzu Maminov <orzu.maminov9@gmail.com>
2023-09-06 15:53:19 +0600

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/countermamin/blockbackend/database"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func main()  {
+	database.Connect()
+	err:=godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port:=os.Getenv("PORT")
+	app:=fiber.New()
+	app.Listen(":"+port)
+}
